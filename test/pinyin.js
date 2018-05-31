e3902664ea991f8cc2f511dc61a426c6e28dca4a
WangTing <365032360@qq.com>
2018-05-31 11:17:06 +0800

    pinyin style for tone as number

--- test/pinyin.js
+++ test/pinyin.js
@@ -1,7 +1,9 @@
 var pinyin = require('pinyin')
 
 let hanzi = '兴海组团青少年活动中心'
-let converted = pinyin(hanzi)
-let string = converted.join("-")
+let converted = pinyin(hanzi, {
+    style: pinyin.STYLE_TONE2,
+})
+let string = converted.join('_')
 console.log(hanzi)
 console.log(string)

1bb102f85b83c44f8fd8722cbbb3de6669aacabe
WangTing <365032360@qq.com>
2018-06-06 11:41:39 +0800

    telnet client test

--- test/tsTest.js
+++ test/tsTest.js
@@ -11,6 +11,14 @@
 //-------------------------------------------------------
 // @ts-ignore
 // const seq = require('../util/utilization').sequenceThrough
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // let s = seq(1, 5)
 // for (const i of s) {
 //     console.log(i)
@@ -25,13 +33,37 @@
 //     }
 // ).join("-")
 // console.log(converted)
-let s = new Map();
-s.set("vlan", 3003);
-s.set("mac", "ACE9-9889-A9C1");
-console.log(s.get("vlan"));
-for (const key of s.keys()) {
-    console.log(key, s.get(key));
+// let s = new Map()
+// s.set("vlan", 3003)
+// s.set("mac", "ACE9-9889-A9C1")
+// console.log(s.get("vlan"))
+// for (const key of s.keys()) {
+//     console.log(key, s.get(key))
+// }
+// let a: number = 0
+// a = s.get('vlan')
+// console.log(a)
+const Telnet = require('telnet-client');
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let connection = new Telnet();
+        let params = {
+            host: '10.254.56.6',
+            port: 23,
+            loginPrompt: /User name:/,
+            passwordPrompt: /User password:/,
+            shellPrompt: /WZCATV/,
+            username: 'wzcatv',
+            password: 'wzcatv703',
+            timeout: 1500
+        };
+        let cnct = yield connection.connect(params);
+        console.log(cnct);
+        let res = yield connection.exec('enable\n');
+        console.log(res)
+        let version0 = yield connection.exec('display version\n');
+        let version1 = yield connection.exec('\n');
+        console.log(version1)
+    });
 }
-let a = 0;
-a = s.get('vlan');
-console.log(a);
+run();
--- test/tsTest.ts
+++ test/tsTest.ts
@@ -29,15 +29,39 @@
 // ).join("-")
 // console.log(converted)
 
-let s = new Map()
-s.set("vlan", 3003)
-s.set("mac", "ACE9-9889-A9C1")
+// let s = new Map()
+// s.set("vlan", 3003)
+// s.set("mac", "ACE9-9889-A9C1")
 
-console.log(s.get("vlan"))
-for (const key of s.keys()) {
-    console.log(key, s.get(key))
+// console.log(s.get("vlan"))
+// for (const key of s.keys()) {
+//     console.log(key, s.get(key))
+// }
+
+// let a: number = 0
+// a = s.get('vlan')
+// console.log(a)
+
+const Telnet = require('telnet-client')
+
+async function run() {
+  let connection = new Telnet()
+
+  let params = {
+    host: '10.254.56.6',
+    port: 23,
+    loginPrompt: /User name:/,
+    passwordPrompt: /User password:/,
+    shellPrompt: /5683/,
+    username: 'wzcatv',
+    password: 'wzcatv703',
+    timeout: 1500
+  }
+
+  let cnct = await connection.connect(params)
+  console.log(cnct)
+
+  let res = await connection.exec('display current\n')
 }
 
-let a: number = 0
-a = s.get('vlan')
-console.log(a)
+run()

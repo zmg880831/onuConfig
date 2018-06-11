ad7a2ead7d2f404bcace2ba67988c6330133e30b
WangTing <365032360@qq.com>
2018-06-11 15:15:53 +0800

    commit node_module update

---
+++ lib/mysql.ts
@@ -0,0 +1,25 @@
+//@ts-ignore
+const mysql = require('mysql')
+
+console.log('try to connect to database onu info')
+let connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '56880707',
+    database:'onu_infomation'
+})
+
+const queryString = 'SELECT * FROM ont'
+//@ts-ignore
+connection.query(queryString, (err, rows, fields) => {
+    if (err) {
+        console.log(err)
+    }
+    //@ts-ignore
+    let onts = rows.map((row) => {
+        return {
+            点位: row.service_name,
+            port: row.port,
+        }
+    })
+})
